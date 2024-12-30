ebb00b79b75680d5c99f9a874dae79b4012272ad
Jakub Chojdak <jchojdak@gmail.com>
2024-12-30 02:42:42 +0100

    Update user service

--- user-service/src/main/java/com/labresults/userservice/service/UserService.java
+++ user-service/src/main/java/com/labresults/userservice/service/UserService.java
@@ -5,17 +5,24 @@ import com.labresults.userservice.dto.UserDTO;
 import com.labresults.userservice.model.User;
 import com.labresults.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final UserRepository repository;
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
     public UserDTO createUser(CreateUserDTO createRequest) {
+        if (userRepository.existsByEmail(createRequest.getEmail())) {
+            // TO-DO
+            //throw new UserAlreadyExistsException();
+        }
 
+        User user = modelMapper.map(createRequest, User.class);
 
-        User savedUser = repository.save();
-        return toDTO(savedUser);
+        User savedUser = userRepository.save(user);
+        return modelMapper.map(savedUser, UserDTO.class);
     }
 }
