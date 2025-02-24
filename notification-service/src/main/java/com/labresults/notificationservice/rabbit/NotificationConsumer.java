99fabb5d4a0c522d250f31f43f98d028fea22a16
Jakub Chojdak <jchojdak@gmail.com>
2025-02-24 17:41:20 +0100

    Add Spring Mail (JavaMailSender)

--- notification-service/src/main/java/com/labresults/notificationservice/rabbit/NotificationConsumer.java
+++ notification-service/src/main/java/com/labresults/notificationservice/rabbit/NotificationConsumer.java
@@ -1,5 +1,6 @@
-package com.labresults.notificationservice.notification;
+package com.labresults.notificationservice.rabbit;
 
+import com.labresults.notificationservice.mail.MailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -7,10 +8,11 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class NotificationConsumer {
-    private final NotificationService service;
+    private final MailService mailService;
 
     @RabbitListener(queues = "notification.queue")
     public void receiveMessage(String message) {
-        service.sendEmail(message);
+        // TO-DO String to, String subject, String text
+        mailService.send("implement.me@gmail.com", "LabResults - notification", message);
     }
 }
